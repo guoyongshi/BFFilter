0ee80362ae743ce2022b952624cd1cf33535d3df
guoyongshi <39519445@qq.com>
2019-10-21 16:51:03 +0800

    test code

--- ui/checkbox.lua
+++ ui/checkbox.lua
@@ -15,15 +15,93 @@ local methods = {
     --文字一行显示不下是否自动调整高度
     ['SetAutoHeight'] = function(self,autoheight)
         self.autoheight = not not autoheight
+    end,
+    ['SetText'] = function(self,text)
+        self.text:SetText(text)
+    end,
+    ['Toggle'] = function(self)
+        self.selected = not self.selected
+        if self.bind_data and self.bind_key then
+            self.bind_data[self.bind_key] = self.selected
+        end
+        if self.selected then
+            self.check:Show()
+        else
+            self.check:Hide()
+        end
     end
 }
 
+local function widget_OnEnter(self)
+    local widget = self.obj
+    widget.highlight:Show()
+end
+
+local function widget_OnLeave(self)
+    local widget = self.obj
+    widget.highlight:Hide()
+end
+
+local function widget_OnMouseDown(self)
+
+end
+
+local function widget_OnMouseUp(self)
+    local widget = self.obj
+    widget:Toggle()
+end
+
+local function widget_Resize(self)
+    local w = self.obj
+    local h = w.text:GetStringHeight()
+    self:SetHeight(h+10)
+end
+
 function BFF_CheckBox(parent,label)
-    local cbx = CreateFrame('CheckButton',nil,parent,'InterfaceOptionsCheckButtonTemplate')
-    cbx.Text:SetText(label)
+    local frame = CreateFrame('Button',nil,parent)
+    frame:EnableMouse(true)
+    frame:SetScript("OnEnter", widget_OnEnter)
+    frame:SetScript("OnLeave", widget_OnLeave)
+    frame:SetScript("OnMouseDown", widget_OnMouseDown)
+    frame:SetScript("OnMouseUp", widget_OnMouseUp)
+    frame:SetScript("OnSizeChanged", widget_Resize)
+
+    --frame:SetBackdrop(BFF_PaneBackdrop)
+    --frame:SetBackdropColor(0.1, 0.1, 0.1, 0.5)
+    --frame:SetBackdropBorderColor(0.4, 0.9, 0.4)
+
+
+    local checkbg = frame:CreateTexture(nil, "ARTWORK")
+    checkbg:SetWidth(24)
+    checkbg:SetHeight(24)
+    checkbg:SetPoint("TOPLEFT")
+    checkbg:SetTexture(130755) -- Interface\\Buttons\\UI-CheckBox-Up
+
+    local check = frame:CreateTexture(nil, "OVERLAY")
+    check:SetAllPoints(checkbg)
+    check:SetTexture(130751) -- Interface\\Buttons\\UI-CheckBox-Check
+    check:Hide()
+
+    local highlight = frame:CreateTexture(nil, "HIGHLIGHT")
+    highlight:SetTexture(130753) -- Interface\\Buttons\\UI-CheckBox-Highlight
+    highlight:SetBlendMode("ADD")
+    highlight:SetAllPoints(checkbg)
+    highlight:Hide()
+
+    local text = frame:CreateFontString(nil,"OVERLAY","GameFontHighlight")
+    text:SetJustifyH("LEFT")
+    text:SetJustifyV("TOP")
+    text:SetNonSpaceWrap(true)
+    text:SetPoint('TOPLEFT',checkbg,'TOPRIGHT',0,0)
+    text:SetHeight(18)  --必须设置一个初始高度，否则计算字符串所需高度时结果是一行的高度
+    text:SetPoint('BOTTOMRIGHT')
+    text:SetText(label or '')
 
     local widget = {
-        frame = cbx,
+        frame = frame,
+        text = text,
+        check = check,
+        highlight = highlight,
         selected = false
     }
 
@@ -33,27 +111,19 @@ function BFF_CheckBox(parent,label)
 
     setmetatable(widget,{__index = BFF_FrameBase})
 
-    cbx:HookScript('OnShow',function (self,...)
+    frame:HookScript('OnShow',function (self,...)
         local w = self.obj
         if w.bind_data and w.bind_key then
             w.selected = not not w.bind_data[w.bind_key]
-            self:SetChecked(w.selected)
+            if w.selected then
+                w.check:Show()
+            else
+                w.check:Hide()
+            end
         end
     end)
 
-    cbx:HookScript('OnClick',function (self,...)
-        local w = self.obj
-        local ck = self:GetChecked()
-        if w.bind_data and w.bind_key then
-            w.bind_data[w.bind_key] = ck
-        end
-        if ck ~= w.selected and w.on_changed_callback then
-            w.selected = ck
-            w:on_changed_callback(ck)
-        end
-    end)
-
-    cbx.obj = widget
+    frame.obj = widget
 
     return widget
 end
--- ui/page_desc.lua
+++ ui/page_desc.lua
@@ -1,7 +1,9 @@
 
 function BFFPage_Desc(parent)
     local box = BFF_ListLayout(parent)
-    local cb = BFF_CheckBox(UIParent,'名字要长。。。。。。。。名字要长。。。。。。。。名字要长。。。。。。。。\n名字要长。。。。。。。。名字要长。。。。。。。。名字要长。。。。。。。。')
+    local cb = BFF_CheckBox(UIParent,'在昨天D组的比赛结束之后，S9小组赛落下帷幕。LPL的IG以及FPX战队进入八强，值得一提的因为FPX是一号种子，IG是二号种子，所以不少观众都担心八强不会内战吧？还好抽签的结果不错，二支队五避免了内战，尽可能的让LPL保留冲击四强的火种。')
+    cb:SetSize(500,32)
+    cb:SetPoint('RIGHT')
 
     box:AddChild(cb)
     for i=1,30 do
